93b2aff0d7c4e6b365f7370ceef5d8bd4e7af365
vero <verofw@hotmail.com>
2023-09-21 00:11:55 -0300

    borre archivo de mas

--- prueba.js
+++
@@ -1,9 +0,0 @@
-const itemElegido = RemerasArt.find((remera) => {
-    return remera.modelo === nombreModelo.toLowerCase() && remera.talle === talle.toLowerCase() && remera.color === color.toLowerCase()
-});
-
-function buscarProducto (categoria){
-    const itemElegido = categoria.find((item) => {
-        return item.modelo === nombreModelo.toLowerCase() && item.talle === talle.toLowerCase() && item.color === color.toLowerCase()
-    });
-}
